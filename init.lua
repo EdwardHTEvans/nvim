e0d8ec6c72bf54aa44897454856903f1c50b5083
Edward Evans <e.evans@uk.parkairsystems.com>
2024-04-03 13:52:09 +0100

    init

---
+++ init.lua
@@ -0,0 +1,38 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- enable 24-bit colour
+vim.opt.termguicolors = true
+
+vim.opt.listchars = { trail = '·', tab = '->' }
+vim.opt.list = true
+
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+
+vim.cmd("colorscheme kanagawa-wave")
+
+require("plugins")
+
+-- setup nvim-tree with options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+require('nvim-cmp')
+require("hotkeys")
+require("lspconfig").clangd.setup{}
+
---
+++ lua/hotkeys.lua
@@ -0,0 +1,41 @@
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+map('n', '<C-b>', ':NvimTreeToggle<CR>', opts)
+map('n', '<C-n>', ':vsp | terminal<CR>i', opts)
+
+map('n', 'ff', ':Telescope find_files<CR>', opts)
+map('n', 'qq', ':q<CR>', opts)
+map('t', '<Esc>', [[<C-\><C-n>]], opts)
+
+map('n', '<S-Left>', '<Esc>v<Left>', opts)
+map('n', '<S-Right>', '<Esc>v<Right>', opts)
+map('n', '<S-Up>', '<Esc>v<Up>', opts)
+map('n', '<S-Down>', '<Esc>v<Down>', opts)
+map('n', '<S-End>', '<Esc>v<End>', opts)
+
+map('i', '<S-Left>', '<Esc>v<Left>', opts)
+map('i', '<S-Right>', '<Esc>v<Right>', opts)
+map('i', '<S-Up>', '<Esc>v<Up>', opts)
+map('i', '<S-Down>', '<Esc>v<Down>', opts)
+map('i', '<S-End>', '<Esc>v<End>', opts)
+
+map('x', '<S-Left>', '<Left>', opts)
+map('x', '<S-Right>', '<Right>', opts)
+map('x', '<S-Up>', '<Up>', opts)
+map('x', '<S-Down>', '<Down>', opts)
+
+map('x', '<Left>', '<Esc><Left>', opts)
+map('x', '<Right>', '<Esc><Right>', opts)
+map('x', '<Up>', '<Esc><Up>', opts)
+map('x', '<Down>', '<Esc><Down>', opts)
+
+map('x', '<C-c>', 'y', opts)
+map('x', '<C-x>', 'd', opts)
+map('x', '<C-x>', 'd', opts)
+
+map('n', '<C-c>', 'yy', opts)
+map('n', '<C-v>', 'P', opts)
+map('n', '<C-z>', 'u', opts)
+map('i', '<C-v>', '<Esc>P', opts)
+map('i', '<C-z>', '<Esc>u', opts)
